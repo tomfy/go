b676a6971e3de6ca2e0864275a75f1c84ff9a2d1
tomfy <tly2@cornell.edu>
2019-10-18 17:12:18 -0400

    slowly adding more stuff ... not there yet.

--- src/chunksearch/chunksearch.go
+++ src/chunksearch/chunksearch.go
@@ -1,57 +1,22 @@
 package main
 
 import (
-	"bufio"
+//	"bufio"
 	"flag"
 	"fmt"
-	"os"
-	"regexp"
-	"strings"
-	"fasta"
+//	"os"
+//	"regexp"
+//	"strings"
+	"seqchunkset"
 )
 
-func store_fasta_file(filename string) int {
-	fh, err := os.Open(filename)
-	if err != nil {
-		os.Exit(1)
-	}
-	
-	var sequences1 []string
-	index_id := make(map[int]string)
-	id_index := make(map[string]int)
-	/*	var chunk_specifier_strings []string  /* elements like '23_51_3_1_135' */
-	min_seq_len := 1000000000
-	max_seq_len := -1
-
-	index := 0
-	scanner1 := bufio.NewScanner(fh)
-	for scanner1.Scan() {
-		line := scanner1.Text()
-		r, _ := regexp.Compile("^>([0-9]+).*")
-		if r.MatchString(line) { /* >id line */
-			match_strings := r.FindStringSubmatch(line)
-			index_id[index] = match_strings[1]
-			id_index[match_strings[1]] = index
-			fmt.Println(match_strings[1])
-		} else { /* sequence line */
-			line = strings.TrimSpace(line)
-			if len(line) < min_seq_len {
-				min_seq_len = len(line)
-			}
-			if len(line) > max_seq_len {
-				max_seq_len = len(line)
-			}
-			sequences1 = append(sequences1, line)
-			fmt.Printf("   [%s]\n", line)
-			index++
-		}
-	}
+/*
 	if min_seq_len != max_seq_len {
 		fmt.Printf("min, max sequence lengths: %8d %8d lengths not equal; exiting.\n", min_seq_len, max_seq_len)
 		os.Exit(1)
 	}
 	return 1
-}
+} */
 
 func main() {
 
@@ -66,45 +31,13 @@ func main() {
 	fmt.Printf("%s\n", *file1Ptr)
 	fmt.Printf("%s\n", *file2Ptr)
 
-	f1retval := store_fasta_file(*file1Ptr)
-	fmt.Printf("%d8\n", f1retval)
+	seqchset := seqchunkset.Construct_from_fasta_file(*file1Ptr)
+	//	fmt.Printf("%3d\n", *f1retval)
+	fmt.Println(*seqchset.Sequences)
+	fmt.Println(*seqchset.Index_id)
+	fmt.Println(*seqchset.Id_index)
 
-/*	var sequences1 []string
-	index_id := make(map[int]string)
-	id_index := make(map[string]int)
-	//	var chunk_specifier_strings []string  // elements like '23_51_3_1_135' 
-	min_seq_len := 1000000000
-	max_seq_len := -1
 
-	index := 0
-	scanner1 := bufio.NewScanner(fh1)
-	for scanner1.Scan() {
-		line := scanner1.Text()
-		r, _ := regexp.Compile("^>([0-9]+).*")
-		if r.MatchString(line) { // >id line 
-			match_strings := r.FindStringSubmatch(line)
-			index_id[index] = match_strings[1]
-			id_index[match_strings[1]] = index
-			fmt.Println(match_strings[1])
-		} else { // sequence line 
-			line = strings.TrimSpace(line)
-			if len(line) < min_seq_len {
-				min_seq_len = len(line)
-			}
-			if len(line) > max_seq_len {
-				max_seq_len = len(line)
-			}
-			sequences1 = append(sequences1, line)
-			fmt.Printf("   [%s]\n", line)
-			index++
-		}
-	}
-	if min_seq_len != max_seq_len {
-		fmt.Printf("min, max sequence lengths: %8d %8d lengths not equal; exiting.\n", min_seq_len, max_seq_len)
-		os.Exit(1)
-	}
-
-	*/
 		
 	/*
 	   	fh2, err := os.Open(*file2Ptr)
--- src/seqchunkset/SCS.go
+++ src/seqchunkset/SCS.go
@@ -1,15 +1,29 @@
-package store
+package seqchunkset
 
-func Store_fasta_file(filename string) int {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type sequence_chunk_set struct {
+	Sequences *[]string
+	Index_id  *map[int]string
+	Id_index  *map[string]int
+}
+
+func Construct_from_fasta_file(filename string) *sequence_chunk_set {
 	fh, err := os.Open(filename)
 	if err != nil {
 		os.Exit(1)
 	}
-	
-	var sequences1 []string
-	index_id := make(map[int]string)
+	var seq_chunk_set sequence_chunk_set
+	var sequences []string
 	id_index := make(map[string]int)
-	/*	var chunk_specifier_strings []string  /* elements like '23_51_3_1_135' */
+	index_id := make(map[int]string)
+	//	var chunk_specifier_strings []string  /* elements like '23_51_3_1_135' */
 	min_seq_len := 1000000000
 	max_seq_len := -1
 
@@ -18,12 +32,12 @@ func Store_fasta_file(filename string) int {
 	for scanner1.Scan() {
 		line := scanner1.Text()
 		r, _ := regexp.Compile("^>([0-9]+).*")
-		if r.MatchString(line) { /* >id line */
+		if r.MatchString(line) { // >id line
 			match_strings := r.FindStringSubmatch(line)
 			index_id[index] = match_strings[1]
 			id_index[match_strings[1]] = index
 			fmt.Println(match_strings[1])
-		} else { /* sequence line */
+		} else { // sequence line
 			line = strings.TrimSpace(line)
 			if len(line) < min_seq_len {
 				min_seq_len = len(line)
@@ -31,7 +45,7 @@ func Store_fasta_file(filename string) int {
 			if len(line) > max_seq_len {
 				max_seq_len = len(line)
 			}
-			sequences1 = append(sequences1, line)
+			sequences = append(sequences, line)
 			fmt.Printf("   [%s]\n", line)
 			index++
 		}
@@ -40,5 +54,8 @@ func Store_fasta_file(filename string) int {
 		fmt.Printf("min, max sequence lengths: %8d %8d lengths not equal; exiting.\n", min_seq_len, max_seq_len)
 		os.Exit(1)
 	}
-	return 1
+	seq_chunk_set.Sequences = &sequences
+	seq_chunk_set.Index_id = &index_id
+	seq_chunk_set.Id_index = &id_index
+	return &seq_chunk_set
 }
