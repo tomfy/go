d953f0531a0b3a61f792950c37dbac7acd65301d
Thomas Fisher-York <tly2@cornell.edu>
2019-10-28 10:57:59 -0400

    n_keep default -> 20

--- src/chunksearch/chunksearch.go
+++ src/chunksearch/chunksearch.go
@@ -12,6 +12,7 @@ import (
 	"runtime/pprof"
 	"seqchunkset"
 	"sequenceset"
+	"sort"
 	"time"
 )
 
@@ -29,7 +30,7 @@ func main() {
 	/* search control parameters */
 	chunk_size := flag.Int("size", 6, "number of snps in each chunk")
 	n_chunks := flag.Int("chunks", -1, "number of chunks to use")
-	n_keep := flag.Int("keep", 16, "# of best matches to keep")
+	n_keep := flag.Int("keep", 20, "# of best matches to keep")
 
 	flag.Parse()
 
@@ -50,6 +51,7 @@ func main() {
 	t0 := time.Now()
 	sequence_set1 := sequenceset.Construct_from_fasta_file(*file1)
 	fmt.Fprintf(os.Stderr, "Done constructing sequence set 1.\n")
+fmt.Fprintf(os.Stderr, "chunk size: %d  n_chunks: %d \n", *chunk_size, *n_chunks)
 	seqchset := seqchunkset.Construct_from_sequence_set(sequence_set1, *chunk_size, *n_chunks)
 	fmt.Fprintf(os.Stderr, "Done constructing sequence chunk set.\n")
 	n_seqs1 := len(seqchset.Sequence_set.Sequences)
@@ -60,38 +62,47 @@ func main() {
 	sequence_set2 := sequenceset.Construct_from_fasta_file(*file2)
 	fmt.Fprintf(os.Stderr, "Done constructing sequence set 2.\n")
 
+	// for each sequence in set 2, search for relatives in set 1
+
 	for index, seq2 := range sequence_set2.Sequences {
-	//	s2 := sequence_set2.Seqs[index]
-		id2 := sequence_set2.Index_to_id(index)	
+		//	s2 := sequence_set2.Seqs[index]
+		id2 := sequence_set2.Index_to_id(index)
 		mindex_count_pairs := make([]mytypes.Pair_int_int, n_seqs1)
 		for i := 0; i < n_seqs1; i++ {
 			mindex_count_pairs[i].A = i
 		}
 
 		top_mindex_count_pairs := seqchset.Get_chunk_matchindex_counts(seq2, mindex_count_pairs, *n_keep)
-		if(index % 500 == 0){
+		if index%500 == 0 {
 			fmt.Fprintf(os.Stderr, "Search %d done.\n", index)
 		}
+		id_matchcount_distance_triples := make([]mytypes.Triple_string_int_double, *n_keep, *n_keep)
 		fmt.Printf("%s   ", id2)
-		for _, mcp := range top_mindex_count_pairs{
+		for i, mcp := range top_mindex_count_pairs {
 			seq1_index := mcp.A
 			seq1_id := sequence_set1.Index_to_id(seq1_index)
 			seq1 := sequence_set1.Sequences[seq1_index]
 			dist12 := distance(seq1, seq2)
-		//	s1 := sequence_set1.Seqs[seq1_index]
-		//	dist12_x := distance_x(s1.Gs, s2.Gs)
-			fmt.Printf("%s %d %6.5f  ", seq1_id, mcp.B, dist12) // , dist12_x) 
+			//	s1 := sequence_set1.Seqs[seq1_index]
+			//	dist12_x := distance_x(s1.Gs, s2.Gs)
+			id_matchcount_distance_triples[i] = mytypes.Triple_string_int_double{seq1_id, mcp.B, dist12}
+			//	fmt.Printf("%s %d %6.5f  ", seq1_id, mcp.B, dist12)
+		}
+		sort.Slice(id_matchcount_distance_triples, func(i, j int) bool { return id_matchcount_distance_triples[i].C < id_matchcount_distance_triples[j].C })
+
+		for _, a_triple := range id_matchcount_distance_triples {
+			fmt.Printf("%s %d %6.5f  ", a_triple.A, a_triple.B, a_triple.C)
 		}
 		fmt.Printf("\n")
 	}
 	t2 := time.Now()
-	
+
 	fmt.Fprintf(os.Stderr, "All searches done.\n")
 	fmt.Printf("# time to search: %v \n", t2.Sub(t1))
 }
 
 func distance_x(seq1 []uint, seq2 []uint) float64 {
-	ok_count := 0 // counts sites where neither seq has missing data
+	ok_count := 0   // counts sites where neither seq has missing data
 	dist_count := 0 // sums differences, i.e. 0-1 -> +=1, 0-2 -> += 2, ...
 	for i := 0; i < len(seq1); i++ {
 		c1 := seq1[i]
@@ -130,16 +141,15 @@ func distance_x(seq1 []uint, seq2 []uint) float64 {
 	}
 	var distance float64
 	if ok_count > 0 {
-		distance = float64(dist_count)/float64(ok_count)
-	}else{
+		distance = float64(dist_count) / float64(ok_count)
+	} else {
 		distance = -1.0 // couldn't calculate because no sites without missing data
 	}
 	return distance
 }
 
-
 func distance(seq1 string, seq2 string) float64 {
-	ok_count := 0 // counts sites where neither seq has missing data
+	ok_count := 0   // counts sites where neither seq has missing data
 	dist_count := 0 // sums differences, i.e. 0-1 -> +=1, 0-2 -> += 2, ...
 	for i := 0; i < len(seq1); i++ {
 		c1 := seq1[i : i+1]
@@ -178,8 +188,8 @@ func distance(seq1 string, seq2 string) float64 {
 	}
 	var distance float64
 	if ok_count > 0 {
-		distance = float64(dist_count)/float64(ok_count)
-	}else{
+		distance = float64(dist_count) / float64(ok_count)
+	} else {
 		distance = -1.0 // couldn't calculate because no sites without missing data
 	}
 	return distance
