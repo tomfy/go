0d0b5e652457522cc904a9f653a22dbcfe2ac812
tomfy <tly2@cornell.edu>
2019-11-01 17:00:26 -0400

    x

--- src/mytypes/mytypes.go
+++ src/mytypes/mytypes.go
@@ -1,8 +1,17 @@
 package mytypes
 
+const MDchar = 'X'
+
 type  Pair_int_int struct {
 	A int
 	B int
+}
+
+type IntIntIntF64 struct {
+	A int
+	B int
+	C int
+	D float64
 }
  
 type Triple_string_int_double struct {
